90a386ac004b92846a83f38a64419febef909ba0
YoelDelRio <yoelvillata95@gmail.com>
2020-12-05 14:02:14 +0100

    commit Inicio

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+var state = false;
+ if(!state){
+ var x =document.getElementById('id_formulario');
+x.style.display="block";
+}
+else{
+	 document.getElementById("id_resultado").style.display="block";
+}
+
